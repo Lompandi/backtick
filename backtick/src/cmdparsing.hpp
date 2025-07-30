d7d29262024b22aac4b4d1aaa8b49383ae09d3e3
Panda Wu <wu.messi.wu@gmail.com>
2025-07-30 12:30:44 +0800

    feat(cmd-parsing): implement g* commands

--- backtick/src/cmdparsing.hpp
+++ backtick/src/cmdparsing.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 void ExecuteHook(const std::string& Command);
+
+struct GCommandParams {
+    bool useHardwareBreakpoint = false;
+    bool hasStartAddress = false;
+    std::uint64_t startAddress = 0;
+    std::vector<std::uint64_t> breakAddresses;
+    std::string breakCommands;
+};
+
+GCommandParams ParseGCommand(const std::string& command);
+
+std::uint64_t ParseAddress(const std::string& addrStr);
